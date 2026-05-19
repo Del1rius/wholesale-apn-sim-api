56cc279859de96c1e1cd69586b3cb62a9488de0b
Del1rius <timothy.aidan.barry@gmail.com>
2026-05-19 10:25:47 +0200

    Created Users views.py and urls.py, Moved serializers.py, urls.py and views.py to api subfolder for cleaner structure

---
+++ users/api/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from users.api.serializers import (
+    UserRegistrationSerializer,
+    UserSerializer,
+    LoginSerializer
+)
+
+
