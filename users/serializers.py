42027a8284eea842f59d462bc48c56451afa616a
Del1rius <timothy.aidan.barry@gmail.com>
2026-05-19 09:54:40 +0200

    Completed Users Serializers

--- users/serializers.py
+++ users/serializers.py
@@ -25,3 +25,47 @@ class UserSerializer(serializers.ModelSerializer):
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
     password_confirm = serializers.CharField(write_only=True, style={'input': 'password'})
+    organization_id = serializers.UUIDField(write_only=True, required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name', 'phone_number', 'role', 'organization_id']
+
+        #Validate passwords match
+        def validate(self, data):
+            if data['password'] != data['passwprd_confirm']:
+                raise serializers.ValidationError({"password": "Passwords must match."})
+            return data
+
+        #Validate Email is unique
+        def validate_email(self, value):
+            if User.objects.filter(email=value).exists():
+                raise serializers.ValidationError("A user with this email already exists.")
+            return value
+        
+        #Create User with hashed password
+        def create(self, validated_data):
+            validated_data.pop('password_confirm')
+            organization_id = validated_data.pop('organization_id', None)
+            password = validated_data.pop('password')
+
+            # Get organization if provided
+            organization = None
+            if organization_id:
+                try:
+                    organization = Organization.objects.get(org_id=organization_id)
+                except Organization.DoesNotExist:
+                    raise serializers.ValidationError({"organization_id": "Organization not found"})
+
+            user = User.objects.create_user(
+                password=password,
+                organization=organization,
+                **validated_data
+            )
+
+            return user
+
+#Serializer for Login validation
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
