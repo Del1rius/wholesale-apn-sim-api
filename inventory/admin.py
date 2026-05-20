0e236eb71ba2f2e3826b6889d6a1c455cc6737dc
Del1rius <timothy.aidan.barry@gmail.com>
2026-05-20 15:11:16 +0200

    Configured Django Admin for Inventory and Users apps

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from .models import APN, SIMCard
 
-# Register your models here.
+
+@admin.register(APN)
+class APNAdmin(admin.ModelAdmin):
+    list_display = ['name', 'apn_string', 'organization', 'authentication_type', 'is_active', 'date_created']
+    list_filter = ['is_active', 'authentication_type', 'organization']
+    search_fields = ['name', 'apn_string', 'organization__name']
+    readonly_fields = ['apn_id', 'date_created', 'date_modified']
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('apn_id', 'name', 'apn_string', 'is_active')
+        }),
+        ('Authentication', {
+            'fields': ('authentication_type', 'username', 'password')
+        }),
+        ('Organization', {
+            'fields': ('organization',)
+        }),
+        ('Timestamps', {
+            'fields': ('date_created', 'date_modified'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(SIMCard)
+class SIMCardAdmin(admin.ModelAdmin):
+    list_display = ['iccid', 'phone_number', 'status', 'carrier', 'network_type', 'organization', 'date_created']
+    list_filter = ['status', 'carrier', 'network_type', 'organization']
+    search_fields = ['iccid', 'phone_number', 'carrier', 'organization__name']
+    readonly_fields = ['sim_id', 'date_created', 'date_modified']
+    
+    fieldsets = (
+        ('SIM Card Information', {
+            'fields': ('sim_id', 'iccid', 'phone_number', 'status')
+        }),
+        ('Network Details', {
+            'fields': ('carrier', 'network_type')
+        }),
+        ('Relationships', {
+            'fields': ('organization', 'apn')
+        }),
+        ('Data Plan', {
+            'fields': ('data_limit_mb', 'activation_date', 'expiry_date')
+        }),
+        ('Additional Information', {
+            'fields': ('notes',),
+            'classes': ('collapse',)
+        }),
+        ('Timestamps', {
+            'fields': ('date_created', 'date_modified'),
+            'classes': ('collapse',)
+        }),
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Organization
 
-# Register your models here.
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'industry', 'contact_email', 'date_created']
+    search_fields = ['name', 'industry', 'contact_email']
+    readonly_fields = ['org_id', 'date_created']
+    
+    fieldsets = (
+        ('Organization Information', {
+            'fields': ('org_id', 'name', 'industry', 'contact_email')
+        }),
+        ('Timestamps', {
+            'fields': ('date_created',),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Fields to display in the user list
+    list_display = ['username', 'email', 'first_name', 'last_name', 'role', 'organization', 'is_staff']
+    list_filter = ['role', 'organization', 'is_staff', 'is_superuser', 'is_active']
+    search_fields = ['username', 'email', 'first_name', 'last_name', 'phone_number']
+    
+    # Customize the user edit form
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'email', 'phone_number')
+        }),
+        ('Organization & Role', {
+            'fields': ('organization', 'role')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            'classes': ('collapse',)
+        }),
+        ('Important Dates', {
+            'fields': ('last_login', 'date_joined'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    # Customize the add user form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'organization', 'role', 'phone_number'),
+        }),
+    )
